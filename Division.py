afe0c192b365dd56e5242834ecc6dd91cf335496
Gopinath Athiappan <109467048+Gopinath-athiappan@users.noreply.github.com>
2022-09-11 22:35:37 +0530

    Division code added

---
+++ Division.py
@@ -0,0 +1,13 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> if __name__ == '__main__':
+...     a = int(input())
+...     b = int(input())
+...     print(a//b)
+...     print (a/b)
+... 
+...     
+654
+40
+16
+16.35
